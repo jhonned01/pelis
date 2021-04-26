6061c6b74c5c2b756653ec2e909d37df331ed330
jhonned01 <jhonned01@gmail.com>
2021-04-26 15:54:33 -0500

    se actualizaron los generos

--- utils/detailMovie.js
+++ utils/detailMovie.js
@@ -1,7 +1,7 @@
 const API_KEY = process.env.API_KEY;
 
 export default {
-  detailMovie: {
+  detailMovieUrl: {
     url: `/${API_KEY}&language=en-US`,
   },
 };
--- utils/requests.js
+++ utils/requests.js
@@ -11,37 +11,39 @@ export default {
     url: `/movie/top_rated?api_key=${API_KEY}&language=es-MX`,
   },
   fetchActionMovies: {
-    title: "Action",
+    title: "Acción",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=es-MX`,
   },
-  fetchComedyMovies: {
-    title: "Comedy",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=es-MX`,
+  fetchBelicas: {
+    title: "Bélica",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10752&language=es-MX`,
+  },
+  fetchCrimen: {
+    title: "Crimen",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=80&language=es-MX`,
+  },
+
+  fetchSciFi: {
+    title: "Sci-Fi",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878&language=es-MX`,
+  },
+  fetchAnimation: {
+    title: "Animación",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16&language=es-MX`,
   },
   fetchHorrorMovies: {
     title: "Horror",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=27&language=es-MX`,
   },
-  fetchRomanceMovies: {
-    title: "Romance",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749&language=es-MX`,
-  },
   fetchMystery: {
-    title: "Mystery",
+    title: "Misterio",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=9648&language=es-MX`,
   },
-  fetchSciFi: {
-    title: "Sci-Fi",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=878&language=es-MX`,
+  fetchComedyMovies: {
+    title: "Comedia",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=es-MX`,
   },
-  fetchWestern: {
-    title: "WesTern",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=37&language=es-MX`,
-  },
-  fetchAnimation: {
-    title: "Animation",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=16&language=es-MX`,
-  },
+
   fetchTv: {
     title: "TV Movie",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=10770&language=es-MX`,
